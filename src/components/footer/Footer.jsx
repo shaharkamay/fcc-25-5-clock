40aa3622bdc42e199a7456c9d0c4f947df94a577
shaharKamay <shahar.kamay@gmail.com>
2021-11-29 13:20:02 +0200

    added templates

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss';
+
+function Footer() {
+    return (
+        <footer>
+            <span> Designed and Coded by </span>
+            <span> Shahar Kamay </span>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1 className="title">25 + 5 Clock</h1>
+        </header>
+    )
+}
+
+export default Header
